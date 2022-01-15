663495ab6154775db580a1916f1ab35a0905f1bc
Roger Puga Ruiz <rogerpugaruiz@gmail.com>
2022-01-15 18:07:43 +0100

    change shadow event

--- js/sortable.js
+++ js/sortable.js
@@ -1,6 +1,29 @@
 
   $( function() {
-    $( "#sortable1, #sortable2, #sortable3" ).sortable({
-      connectWith: ".connectedSortable"
+    $( "#sortable1, #sortable3" ).sortable({
+      connectWith: ".connectedSortable",
+      stop: function( event, ui ) {
+        ui.item.css("box-shadow", "2px 2px black");
+        console.log(ui.item)
+      },
+      start: function( event, ui ) {
+        ui.item.css("box-shadow", "10px 10px black");
+        console.log(ui.item)
+      }
     }).disableSelection();
   } );
+
+  $(function(){
+    $("#sortable2").sortable(
+      {
+      stop: function( event, ui ) {
+        ui.item.css("box-shadow", "2px 2px black");
+        console.log(ui.item)
+      },
+      start: function( event, ui ) {
+        ui.item.css("box-shadow", "10px 10px black");
+        console.log(ui.item)
+      }
+      }
+    );
+  });
